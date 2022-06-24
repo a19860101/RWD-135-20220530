fc1ada8e18a10c0f73bf8c602b98f762a18c85a3
Zac <a19860101@gmail.com>
2022-06-24 21:30:05 +0800

    20220624

--- JAVASCRIPT/variable.js
+++ JAVASCRIPT/variable.js
@@ -1,4 +1,4 @@
-// 變數
+// 變數 variable
 /* 
     命名規範
     1. 首字只可使用英文或底線
@@ -8,9 +8,30 @@
 var a = 100;
 var b = 'hello javascript~!';
 
-console.log(b);
+var a = 333;
+
+console.log(a);
 
 let x = 99;
 let y = 'hello let!!';
 
+x = 100;
+
 console.log(x,y);
+
+
+// 常數 constant variable
+
+const q = 'qwerty!!';
+
+console.log(q);
+
+/* 
+    var vs let,const
+
+    var可以重複宣告；let,const不行
+    const不可以重新賦值
+    var是function scope；let,const是block scope
+
+*/
+
