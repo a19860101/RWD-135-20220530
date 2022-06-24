72d5102c24932dbfc5c90fffc33ec2c48372290a
Zac <a19860101@gmail.com>
2022-06-24 21:54:38 +0800

    20220624

---
+++ JAVASCRIPT/condition.js
@@ -0,0 +1,30 @@
+//條件式
+
+// if...
+let x = -10;
+if(x > 0){
+    console.log('success');
+}
+
+// if...else
+if(x > 0){
+    console.log('success 2');
+}else{
+    console.log('error 2');
+}
+
+// if...else if
+if(x > 0){
+    console.log('正數');
+}else if(x < 0){
+    console.log('負數');
+}
+
+// if..else if...else
+if(x > 0){
+    console.log('正');
+}else if(x < 0){
+    console.log('負');
+}else{
+    console.log(0)
+}
