cd087bc506d540941d88c0cbbf9ae0979b119b5d
Zac <a19860101@gmail.com>
2022-06-24 21:19:19 +0800

    20220624

---
+++ JAVASCRIPT/variable.js
@@ -0,0 +1,16 @@
+// 變數
+/* 
+    命名規範
+    1. 首字只可使用英文或底線
+    2. 大小寫有別
+    3. 不可使用保留字
+*/
+var a = 100;
+var b = 'hello javascript~!';
+
+console.log(b);
+
+let x = 99;
+let y = 'hello let!!';
+
+console.log(x,y);
