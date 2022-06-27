4fbfefb5038cb524f93d5745e557831ab0a4ec51
Zac <a19860101@gmail.com>
2022-06-27 20:19:58 +0800

    20220627

--- JAVASCRIPT/array2.js
+++ JAVASCRIPT/array2.js
@@ -27,3 +27,7 @@ console.log(drink_join);
 // toString() 陣列->字串
 let drink_str = drinks.toString('_');
 console.log(drink_str)
+
+// split()字串->陣列
+let str = '礦泉水,阿薩姆紅茶,茉莉綠茶,美式咖啡,拿鐵';
+console.log(str.split(','))
