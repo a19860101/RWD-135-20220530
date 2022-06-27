bcc39fc617a8c811bdf0cd4869172c943c14db98
Zac <a19860101@gmail.com>
2022-06-27 19:40:37 +0800

    20220627

--- JAVASCRIPT/array.js
+++ JAVASCRIPT/array.js
@@ -10,8 +10,24 @@
 
 let drinks = ['礦泉水','阿薩姆紅茶','茉莉綠茶','美式咖啡'];
 
-console.log(drinks);
-console.log(drinks[0]);
-console.log(drinks[1]);
-console.log(drinks[2]);
-console.log(drinks[3]);
+// console.log(drinks);
+// console.log(drinks[0]);
+// console.log(drinks[1]);
+// console.log(drinks[2]);
+// console.log(drinks[3]);
+
+console.log(drinks.length);
+
+
+//陣列迭代
+// for(let i=0;i<drinks.length;i++){
+//     console.log(drinks[i])
+// }
+
+// for(let data in drinks){
+    // console.log(data);
+    // console.log(drinks[data]);
+// }
+for(let data of drinks){
+    console.log(data);
+}
