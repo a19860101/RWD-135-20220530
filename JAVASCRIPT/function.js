d98ab895d14bccac2d1a2a3220119eef64ec1b1f
Zac <a19860101@gmail.com>
2022-06-29 19:09:52 +0800

    20220629

---
+++ JAVASCRIPT/function.js
@@ -0,0 +1,13 @@
+// 函式宣告式 Function declaration
+function test(){
+    let x = 100;
+    // console.log(x);
+    return x;
+}
+
+// 函式表達式 Function expression
+let test2 = function(){
+    let y = 999;
+    return y;
+}
+console.log(test2());
