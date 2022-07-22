39a843088c085ce6166768d3ce715a2747cd1542
Zac <a19860101@gmail.com>
2022-07-22 21:52:02 +0800

    20220722

--- TAILWIND_CLI/tailwind.config.js
+++ TAILWIND_CLI/tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily:{
+      sans: ['Nunito','Noto Sans TC'],
+      serif: ['georgia','標楷體']
+    },
+    screens: {
+      sm:'480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    extend: {
+      colors:{
+        primary: {
+          '100': 'blue',
+          '200': '#987988'
+        },
+        secondary: '#999'
+      }
+    },
   },
   plugins: [],
 }
